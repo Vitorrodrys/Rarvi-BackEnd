9d4e2d4f50453ee71890432d97039b4d8e10feb3
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-04-25 10:52:34 -0300

    Fix: typo in 'env_setings'

--- src/core/log.py
+++ src/core/log.py
@@ -3,9 +3,9 @@ import logging
 from core import settings
 
 def init_logging(log_level: settings.LogLevelsEnum = None) -> None:
-    env_setings = settings.get()
+    env_settings = settings.get()
 
-    log_level = log_level or env_setings.LOG_LEVEL
+    log_level = log_level or env_settings.LOG_LEVEL
     logger = logging.getLogger()
     logger.setLevel(log_level)
     logger.info("Global log level set to %s", log_level)
